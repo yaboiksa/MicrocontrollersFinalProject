f84d9d37e71d042f542471ae1c9060988e1fcc19
yaboiksa <ky.samuel1@gmail.com>
2021-11-11 12:55:31 -0500

    Start on cpp which can be transfered to ino

---
+++ ArduinoRunFiles/Connect4.cpp
@@ -0,0 +1,37 @@
+using namespace std;
+
+
+enum Player{
+    Player1,
+    Player2
+};
+
+struct Surrounding
+{
+    int left, right, up, upperLeft, upperRight;
+};
+
+class Piece{
+    // I. Storage of Player, storage of # of surrounding pieces
+    // II. Getters
+
+    private:
+    Player p;
+    Surrounding s;
+
+    public:
+    
+};
+
+class Board{
+
+
+
+
+};
+
+
+int main(int argv, char** argc){
+
+
+}
