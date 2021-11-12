8fa894a1a08b7e0a58cc156a5ee65d7e1b007efd
yaboiksa <ky.samuel1@gmail.com>
2021-11-12 15:45:42 -0500

    Global variables

--- ArduinoFiles/Uno.ino
+++ ArduinoFiles/Uno.ino
@@ -5,8 +5,23 @@ enum Player{
 };
 
 Player Board[7][7];
+int Heights[7] = {0, 0, 0, 0, 0, 0, 0};
+Player currPlayer;
 
+void putPiece(int Column){
+    // I. Check which player is placing the piece
+    // II. Check if win condition is met
+    if(currPlayer == Player1){
+        
+    }
+    else {
 
+    }
+}
+
+void checkWin(int y, int x){
+
+}
 
 
 void setup(){}
