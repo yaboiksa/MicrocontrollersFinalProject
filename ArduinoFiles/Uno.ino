a01101065cc3ec8a70430ce3f40f065532cd85de
yaboiksa <ky.samuel1@gmail.com>
2021-11-12 16:05:56 -0500

    Finished putPiece --- Needs testing

--- ArduinoFiles/Uno.ino
+++ ArduinoFiles/Uno.ino
@@ -6,21 +6,36 @@ enum Player{
 
 Player Board[7][7];
 int Heights[7] = {0, 0, 0, 0, 0, 0, 0};
-Player currPlayer;
+Player currPlayer = Player1;
 
 void putPiece(int Column){
-    // I. Check which player is placing the piece
+    // I. Place the Piece
     // II. Check if win condition is met
-    if(currPlayer == Player1){
-        
-    }
-    else {
+    // III. Switch which player
 
+    int currX = Column;
+    int currY = Heights[Column];
+
+    // I. Place the Piece
+    Board[currY][currX] = currPlayer;
+    Heights[Column] += 1;
+
+    // II. Check if win condition is met
+    checkWin(currY, currX);
+
+    // III. Switch which player
+    if (currPlayer == Player1){
+        currPlayer = Player2;
+    }
+    else{
+        currPlayer = Player1;
     }
 }
 
 void checkWin(int y, int x){
-
+    // I. Check everything
+    //      a.
+    // II. If won output winscreen
 }
 
 
