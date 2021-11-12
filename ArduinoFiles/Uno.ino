8f22be88fefbbdeb0b1254720b00f7790409efe4
yaboiksa <ky.samuel1@gmail.com>
2021-11-12 16:52:50 -0500

    Check down and horizontal - Done, need testing

--- ArduinoFiles/Uno.ino
+++ ArduinoFiles/Uno.ino
@@ -13,8 +13,8 @@ void putPiece(byte Column){
     // II. Check if win condition is met
     // III. Switch which player
 
-    byte currX = Column;
-    byte currY = Heights[Column];
+    int currX = Column; // refactor to byte later
+    int currY = Heights[Column]; // refactor to byte later
 
     // I. Place the Piece
     Board[currY][currX] = currPlayer;
@@ -38,17 +38,67 @@ void checkWin(byte y, byte x){
     //      b. Check horizontal
     //      c. Check left diagonal
     //      d. Check right diagonal
+    //          }Check left side
+    //          } And check right side
+    //          } And check if at endpoints
     // II. If won output win
+
+    int connectedPieces; // refactor to byte later
+    int i; // refactor to byte later
+    bool dir = false; // refactor?
 
     // I. Check everything
     //      a. Check down
-    if (Heights[x] == 4){
-        for (byte i = 0; i < count; i++)
-        {
-            /* code */
+    if (Heights[x] >= 4){
+        connectedPieces = 1;
+        for (i = 1; i < 5; i++){
+            if(Board[y - i][x] != currPlayer){
+                break;
+            }
+            connectedPieces++;
         }
         
+        if (connectedPieces == 4){
+            // Win
+        }
     }
+    //      b. Check horizontal
+    connectedPieces = 1;
+    if ((x >= 0) || (x <= 6)){
+        if (x == 0){
+            dir = true;
+        }
+
+        for ( i = 1; i < 5; i++){
+            //  }Check left side
+            if((Board[y][x - i] == currPlayer) && dir == false){
+                connectedPieces++;
+            }
+            else{
+                dir = true;
+            }
+            
+
+            //  }Check right side
+            if((Board[y][x + i] == currPlayer) && dir == true){
+                connectedPieces++;
+            }
+            else{
+                dir = false;
+            }
+
+
+            // } Check if at endpoints
+            if ((x == 0) || (x - i == 0))
+                    dir = true;
+            if ((x == 6) || (x - i == 6))
+                    dir = false;
+        }
+    }
+
+
+    
+
 
 
 
